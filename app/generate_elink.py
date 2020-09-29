0c99de1158446e7987cc024907132812e9ca806a
Rongkun Wang <rongkun.wang@cern.ch>
2020-09-29 12:18:44 +0200

    update generate elink, and move

--- app/generate_elink.py
+++ app/generate_elink.py
@@ -13,6 +13,8 @@ def fun(flxid, card, l_links, phase = 1):
         all_elinks = [8, 16, 24, 33]
         if phase == 2:
             all_elinks = [11, 19, 25, 33]
+        if phase == 3:
+            all_elinks = [11, 19, 27, 35]
         for i in all_elinks:
             l_elink_flx.append( card * 2 ** 11 + link * 2**6 + i)
             l_elink.append(     flxid * 2**16 + card * 2 ** 11 + link * 2**6 + i)
@@ -32,14 +34,16 @@ def fun(flxid, card, l_links, phase = 1):
 #  fun(0, 0, range(12),     1)
 #  fun(0, 1, [6, 7, 9, 10], 1)
 #  fun(0, 3, range(4) + range(6, 10), 1)
-fun(1, 0, range(12),     1)
-fun(1, 1, [6, 7, 9, 10], 1)
-fun(1, 3, range(4) + range(6, 10), 1)
+#  fun(1, 0, range(12),     1)
+#  fun(1, 1, [6, 7, 9, 10], 1)
+#  fun(1, 3, range(4) + range(6, 10), 1)
 
 # phase 2
 #  fun(0, 0, range(12),     2)
 #  fun(0, 1, [6, 7, 9, 10], 2)
 #  fun(0, 3, range(4) + range(6, 10), 2)
-fun(1, 0, range(12),     2)
-fun(1, 1, [6, 7, 9, 10], 2)
-fun(1, 3, range(4) + range(6, 10), 2)
+#  fun(1, 0, range(12),     2)
+#  fun(1, 1, [6, 7, 9, 10], 2)
+#  fun(1, 3, range(4) + range(6, 10), 2)
+
+fun(0, 0, range(12),     3)
